a886c1c3889eff90a38359ffc8fca54f4dca1031
Ariana Silva <arianarebeca.silva@gmail.com>
2024-06-07 15:05:32 -0300

    add: findUser Ok e updateUser em andamento

--- testes-api/cypress/e2e/findUser.cy.js
+++ testes-api/cypress/e2e/findUser.cy.js
@@ -1,5 +1,4 @@
 import { faker } from "@faker-js/faker"
-import { criaUsuario } from "../support/usuario";
 
 describe('Testes da funcionalidade de encontrar usuários', () => {
     let userName;
@@ -10,7 +9,7 @@ describe('Testes da funcionalidade de encontrar usuários', () => {
 
     beforeEach(() => {
         userName = faker.person.fullName();
-        userEmail = faker.internet.email();
+        userEmail = faker.internet.email().toLowerCase();;
         password = faker.internet.password({ length: 10 });
 
         cy.createUser(userName, userEmail, password).then((response) => {
@@ -37,7 +36,7 @@ describe('Testes da funcionalidade de encontrar usuários', () => {
             expect(response.status).to.equal(200);
             expect(response.body.id).to.equal(uId);
             expect(response.body.name).to.equal(userName);
-            // expect(response.body.email).includes(userEmail); aqui tá quebrando por causa das iniciais do email
+            expect(response.body.email).includes(userEmail);
             expect(response.body).to.have.property('type');
             expect(response.body).to.have.property('active');
         })
@@ -65,41 +64,85 @@ describe('Testes da funcionalidade de encontrar usuários', () => {
     })
 
     it('Não deve ser possível encontrar usuário por meio de outros dados', () => {
+        cy.promoteAdmin();
         cy.request({
             method: 'GET',
             url: 'users/' + userEmail,
             headers: {
                 Authorization: `Bearer ${uToken}`
-            },
+            }, failOnStatusCode: false
         }).then((response) => {
-    
+            cy.log(response)
+            expect(response.body.statusCode).to.equal(400);
+            expect(response.body.error).to.equal("Bad Request");
+            expect(response.body.message).to.equal("Validation failed (numeric string is expected)");
         })
     })
 
-    it('Não deve ser possível encontrar usuário com o campo em branco', () => {
-
-    })
-
-    it.only('Um usuário comum não deve encontrar outros cadastros', () => {
-        let id;
-        cy.createUser(faker.person.firstName(), faker.internet.email(), '123456').then((response) => {
-            id = response.body.id;
-            cy.log(id)
+    it('Um usuário do tipo comum não deve encontrar outros usuários', () => {
+        let name = faker.person.firstName();
+        let email = faker.internet.email().toLowerCase();
+        cy.createUser(name, email, '1234567').then((response) => {
+            cy.wrap(response.body).as('body')
         });
-        cy.request({
-            method: 'GET',
-            url: 'users/' + id,
-            headers: {
-                Authorization: `Bearer ${uToken}`
-            },
-            failOnStatusCode: false
-        }).then((response) => {
-            expect(response.status).to.equal(403);
-            expect(response.body.message).to.equal("Forbidden");
+        cy.get('@body').then((body) => {
+            cy.request({
+                method: 'GET',
+                url: 'users/' + body.id,
+                headers: {
+                    Authorization: `Bearer ${uToken}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(403);
+                expect(response.body.message).to.equal("Forbidden");
+            })
         })
     })
 
-    it('Apenas um usuário do tipo admin pode encontrar outros cadastros', () => {
+    it('Um usuário do tipo crítico não deve encontrar outros usuários', () => {
+        let name = faker.person.firstName();
+        let email = faker.internet.email().toLowerCase();
+        cy.promoteCritic();
+        cy.createUser(name, email, '1234567').then((response) => {
+            cy.wrap(response.body).as('body')
+        });
+        cy.get('@body').then((body) => {
+            cy.request({
+                method: 'GET',
+                url: 'users/' + body.id,
+                headers: {
+                    Authorization: `Bearer ${uToken}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(403);
+                expect(response.body.message).to.equal("Forbidden");
+                expect(response.body.statusCode).to.equal(403);
+            })
+        })
+    })
 
+    it('Apenas um usuário do tipo admin pode encontrar outros usuários', () => {
+        let name = faker.person.firstName();
+        let email = faker.internet.email().toLowerCase();
+        cy.promoteAdmin();
+        cy.createUser(name, email, '1234567').then((response) => {
+            cy.wrap(response.body).as('body')
+        });
+        cy.get('@body').then((body) => {
+            cy.request({
+                method: 'GET',
+                url: 'users/' + body.id,
+                headers: {
+                    Authorization: `Bearer ${uToken}`
+                },
+            }).then((response) => {
+                expect(response.status).to.equal(200);
+                expect(response.body.id).to.equal(body.id);
+                expect(response.body.name).to.equal(name);
+                expect(response.body.email).to.equal(email);
+                expect(response.body).to.have.property('type');
+                expect(response.body).to.have.property('active');
+            })
+        })
     })
 })
--- testes-api/cypress/e2e/updateUser.cy.js
+++ testes-api/cypress/e2e/updateUser.cy.js
@@ -1,39 +1,137 @@
 import { faker } from "@faker-js/faker"
 
 describe('Testes da funcionalidade de atualizar usuários', () => {
+    let userName;
+    let userEmail;
+    let password;
+    let uId;
+    let uToken;
 
+    beforeEach(() => {
+        userName = faker.person.fullName();
+        userEmail = faker.internet.email().toLowerCase();;
+        password = faker.internet.password({ length: 10 });
 
-    beforeEach(() =>{
-
+        cy.createUser(userName, userEmail, password).then((response) => {
+            uId = response.body.id;
+        })
+        cy.login(userEmail, password).then((response) => {
+            uToken = response.body.accessToken;
+        })
     })
 
     afterEach(() => {
-
+        cy.promoteAdmin();
+        cy.deleteUser(uId, uToken);
     })
 
-    it('Deve ser possível atualizar um usuário com sucesso', () => {
-
-    })
-    
-    it('Não deve ser possível atualizar outra contas como um usuário comum', () => {
-
+    it('Deve ser possível atualizar o próprio nome e senha com sucesso', () => {
+        cy.request({
+            method: 'PUT',
+            url: 'users/' + uId,
+            body: {
+                name: 'Zelda',
+                password: '252525'
+            },
+            headers: {
+                Authorization: `Bearer ${uToken}`
+            },
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.body.name).to.equal('Zelda')
+            expect(response.body.email).to.equal(userEmail)
+        })
     })
 
     it('Deve ser possível atualizar outras contas como um usuário admin', () => {
+        let name = faker.person.firstName();
+        let email = faker.internet.email().toLowerCase();
 
+        cy.promoteAdmin();
+        cy.createUser(name, email, '1234567').then((response) => {
+            cy.wrap(response.body).as('body')
+        });
+        cy.get('@body').then((body) => {
+            cy.request({
+                method: 'PUT',
+                url: 'users/' + body.id,
+                body: {
+                    name: 'Zelda',
+                    password: '252525'
+                },
+                headers: {
+                    Authorization: `Bearer ${uToken}`
+                },
+            }).then((response) => {
+                expect(response.body.name).to.equal('Zelda');
+                expect(response.body.email).to.equal(email);
+                expect(response.body.id).to.equal(body.id);
+                expect(response.body).to.have.property('type');
+                expect(response.body).to.have.property('active');
+            })
+        })
     })
 
-    it('Não deve ser possível atualizar outras contas Como um usuário critico', () => {
+    it('Não deve ser possível atualizar outra contas como um usuário comum', () => {
+        let name = faker.person.firstName();
+        let email = faker.internet.email().toLowerCase();
+
+        cy.createUser(name, email, '1234567').then((response) => {
+            cy.wrap(response.body).as('body')
+        });
+        cy.get('@body').then((body) => {
+            cy.request({
+                method: 'PUT',
+                url: 'users/' + body.id,
+                body: {
+                    name: 'Zelda',
+                    password: '252525'
+                },
+                headers: {
+                    Authorization: `Bearer ${uToken}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(403);
+                expect(response.body.message).to.equal("Forbidden");
+            })
+        })
+    })
+
+
+    it('Não deve ser possível atualizar outras contas como um usuário critico', () => {
+        let name = faker.person.firstName();
+        let email = faker.internet.email().toLowerCase();
         
+        cy.promoteCritic();
+        cy.createUser(name, email, '1234567').then((response) => {
+            cy.wrap(response.body).as('body')
+        });
+        cy.get('@body').then((body) => {
+            cy.request({
+                method: 'PUT',
+                url: 'users/' + body.id,
+                body: {
+                    name: 'Zelda',
+                    password: '252525'
+                },
+                headers: {
+                    Authorization: `Bearer ${uToken}`
+                }, failOnStatusCode: false
+            }).then((response) => {
+                expect(response.status).to.equal(403);
+                expect(response.body.message).to.equal("Forbidden");
+            })
+        })
+
     })
 
     it('Não deve ser possível deixar campos sem informação', () => {
-        
+
     })
     it('O novo nome precisa estar entre 1 e 100 caracteres', () => {
-        
+
     })
     it('A nova senha precisa estar entre 6 a 12 caracteres', () => {
-        
+
     })
 })
