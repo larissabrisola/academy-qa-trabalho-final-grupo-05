c2a818bf55eef365193bcb4868d462c9e4dd4079
Manoel Porto <86935666+manoscreu@users.noreply.github.com>
2024-06-09 12:39:36 -0300

    Update testes-api/cypress/e2e/promoteUserToCritic.cy.js
    Co-authored-by: arianarsilva <165231069+arianarsilva@users.noreply.github.com>

--- testes-api/cypress/e2e/promoteUserToCritic.cy.js
+++ testes-api/cypress/e2e/promoteUserToCritic.cy.js
@@ -34,7 +34,7 @@ describe("Teste promover usuário a crítico", () => {
             expect(response.status).to.equal(204)
         })
     })
-    it("Caso um usuário ja tenha uma review feita anteriormente apos a promoção a review não deve ser alterada", () => {
+    it("A review anterior não deve ser alterada após a promoção do usuário a critic", () => {
         let idMovie
         let token
         cy.promoteAdmin()
