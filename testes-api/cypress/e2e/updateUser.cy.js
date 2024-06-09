1d64ce2b33e8e87d746cdbf5ff89a2ecf10afdf1
Ariana Silva <arianarebeca.silva@gmail.com>
2024-06-09 16:10:23 -0300

    revisão: updateUser finalizado

--- testes-api/cypress/e2e/updateUser.cy.js
+++ testes-api/cypress/e2e/updateUser.cy.js
@@ -43,6 +43,42 @@ describe('Testes da funcionalidade de atualizar usuários', () => {
         })
     })
 
+    it('Deve ser possível atualizar o próprio nome', () => {
+        cy.request({
+            method: 'PUT',
+            url: 'users/' + uId,
+            body: {
+                name: 'Zelda',
+                password: password
+            },
+            headers: {
+                Authorization: `Bearer ${uToken}`
+            },
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.body.name).to.equal('Zelda')
+            expect(response.body.email).to.equal(userEmail)
+        })
+    })
+
+    it('Deve ser possível atualizar a própria senha', () => {
+        cy.request({
+            method: 'PUT',
+            url: 'users/' + uId,
+            body: {
+                name: userName,
+                password: '2525252'
+            },
+            headers: {
+                Authorization: `Bearer ${uToken}`
+            },
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.body.name).to.equal(userName)
+            expect(response.body.email).to.equal(userEmail)
+        })
+    })
+
     it('Deve ser possível atualizar outras contas como um usuário admin', () => {
         let name = faker.person.firstName();
         let email = faker.internet.email().toLowerCase();
@@ -123,7 +159,7 @@ describe('Testes da funcionalidade de atualizar usuários', () => {
         })
     })
 
-    it('Não deve ser possível deixar campo nome sem informação (sem o mínimo valor de caracteres)', () => {
+    it('Não deve ser possível atualizar usuário sem informar nome', () => {
         cy.request({
             method: 'PUT',
             url: 'users/' + uId,
@@ -142,7 +178,7 @@ describe('Testes da funcionalidade de atualizar usuários', () => {
         })
     })
 
-    it('Não deve ser possível deixar campo senha sem informação (sem o mínimo valor de caracteres)', () => {
+    it('Não deve ser possível atualizar usuário sem informar senha', () => {
         cy.request({
             method: 'PUT',
             url: 'users/' + uId,
@@ -161,8 +197,7 @@ describe('Testes da funcionalidade de atualizar usuários', () => {
         })
     })
 
-
-    it('O novo nome precisa ter no máximo 100 caracteres', () => {
+    it('Não deve ser possível que o novo nome tenha acima de 100 caracteres', () => {
         cy.request({
             method: 'PUT',
             url: 'users/' + uId,
@@ -180,7 +215,28 @@ describe('Testes da funcionalidade de atualizar usuários', () => {
         })
     })
 
-    it('A nova senha precisa ter no máximo 12 caracteres', () => {
+    it('Deve ser possível que o novo nome tenha 100 caracteres', () => {
+        let name = 'a'.repeat(100);
+        cy.request({
+            method: 'PUT',
+            url: 'users/' + uId,
+            body: {
+                name: name,
+                password: '252525'
+            },
+            headers: {
+                Authorization: `Bearer ${uToken}`
+            },
+        }).then((response) => {
+            expect(response.body.name).to.equal(name);
+            expect(response.body.email).to.equal(userEmail);
+            expect(response.body.id).to.equal(uId);
+            expect(response.body).to.have.property('type');
+            expect(response.body).to.have.property('active');
+        })
+    })
+
+    it('Não deve ser possível que a nova senha tenha mais que 12 caracteres', () => {
         cy.request({
             method: 'PUT',
             url: 'users/' + uId,
@@ -195,6 +251,70 @@ describe('Testes da funcionalidade de atualizar usuários', () => {
             expect(response.body.statusCode).to.equal(400)
             expect(response.body.error).to.equal("Bad Request")
             expect(response.body.message[0]).to.equal('password must be shorter than or equal to 12 characters')
+        })
+    })
+
+    it('Deve ser possível que a nova senha tenha 12 caracteres', () => {
+        cy.request({
+            method: 'PUT',
+            url: 'users/' + uId,
+            body: {
+                name: 'Zelda',
+                password: '2'.repeat(12)
+            },
+            headers: {
+                Authorization: `Bearer ${uToken}`
+            }, failOnStatusCode: false
+        }).then((response) => {
+            expect(response.body.name).to.equal('Zelda');
+            expect(response.body.email).to.equal(userEmail);
+            expect(response.body.id).to.equal(uId);
+            expect(response.body).to.have.property('type');
+            expect(response.body).to.have.property('active');
+        })
+    })
+
+    it('Mesmo inserindo novo e-mail no body da requisição, o dado não deve ser atualizado', () => {
+        cy.request({
+            method: 'PUT',
+            url: 'users/' + uId,
+            body: {
+                name: userName,
+                password: '252525',
+                email: 'lurdes@gmail.com'
+            },
+            headers: {
+                Authorization: `Bearer ${uToken}`
+            },
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.body.name).to.equal(userName);
+            expect(response.body.email).to.equal(userEmail);
+            expect(response.body.id).to.equal(uId);
+            expect(response.body).to.have.property('type');
+            expect(response.body).to.have.property('active');
+        })
+    })
+
+    it.only('Não deve ser possível alterar o tipo do usuário', () => {
+        cy.request({
+            method: 'PUT',
+            url: 'users/' + uId,
+            body: {
+                name: userName,
+                password: password,
+                type: 1
+            },
+            headers: {
+                Authorization: `Bearer ${uToken}`
+            },
+        }).then((response) => {
+            expect(response.status).to.equal(200)
+            expect(response.body.name).to.equal(userName);
+            expect(response.body.email).to.equal(userEmail);
+            expect(response.body.id).to.equal(uId);
+            expect(response.body.type).to.equal(0);
+            expect(response.body).to.have.property('active');
         })
     })
 })
