2911a0beba587a748c9e531c47d03fd6054f7cab
Ariana Silva <arianarebeca.silva@gmail.com>
2024-06-11 23:38:44 -0300

    feature: iniciando testes de deleteMovie

---
+++ testes-api/cypress/e2e/deleteMovie.cy.js
@@ -0,0 +1,24 @@
+import { faker } from "@faker-js/faker"
+
+describe('Deletar filme', () =>{
+    
+    it('Deve ser possível deletar filme como um usuário do tipo comum', () => {
+
+    })
+
+    it('Deve ser possível deletar filme como um usuário do tipo admin', () => {
+
+    })
+
+    it('Deve ser possível deletar filme como um usuário do tipo critic', () => {
+
+    })
+
+    it('Não deve ser possível deletar filme sem estar autenticado no sistema', () => {
+
+    })
+
+    it('Não deve ser possível deletar filme informando título', () => {
+        
+    })
+})
