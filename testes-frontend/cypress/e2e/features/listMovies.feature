754137a907d6fa7962715055e94b4070e019c0e7
larissabrisola <alarissabrisola@gmail.com>
2024-06-13 15:42:20 -0300

    inicio testes listMovies

--- testes-frontend/cypress/e2e/features/listMovies.feature
+++ testes-frontend/cypress/e2e/features/listMovies.feature
@@ -1,31 +1,40 @@
-Feature: Listagem de filmes
+Feature: Lista de filmes
 
     Background: usuário está na tela inicial 
         Given usuário está na tela inicial 
 
-    # Scenario: Deve ser possivel visualizar duas categorias de filmes: em destaque e mais bem avaliados
-    #     When existir filmes cadastrados 
-    #     Then deve ser possivel visualizar duas categorias de filmes
+    Scenario: Deve ser possivel visualizar duas categorias de filmes: em destaque e mais bem avaliados
+        When existir filmes cadastrados 
+        Then deve ser possivel visualizar duas categorias de filmes
+
     Scenario: Deve ser possivel visualizar titulo, descrição, capa e nota dos filmes
         When existir filmes cadastrados 
-        Then deve ser possivel visualizar as informações dos filmes 
-    # Scenario: Deve ser possivel navegar entre os filmes em destaque
-    #     When existir filmes cadastrados 
-    # Scenario: Deve ser possivel navegar entre os filmes mais bem avaliados
-    #     When existir filmes cadastrados 
-    # Scenario: Deve ser possivel consultar mais detalhes do filme ao seleciona-lo
-    #     When existir filmes cadastrados 
-    # Scenario: Deve ser exibido um aviso quando não existir filmes cadastrados
-    #     When não existir filmes cadastrados 
-    #     Then um aviso será exibido
+         Then deve ser possivel visualizar as informações dos filmes 
+  
+    Scenario: Deve ser possivel navegar entre os filmes em destaque
+        When existir filmes cadastrados 
+        And clicar no botão de próximo em filmes em destaque
+        Then deve ser possivel visualizar mais filmes 
+    
+    Scenario: Deve ser possivel navegar entre os filmes mais bem avaliados
+        When existir filmes cadastrados 
+        And clicar no botão de próximo em filmes mais bem avaliados
+        Then deve ser possivel visualizar mais filmes
+
+    Scenario: Deve ser possivel consultar mais detalhes do filme ao seleciona-lo
+        When existir filmes cadastrados 
+        And clicar em um filme 
+        Then usuário será redirecionado para a tela de detalhes do filme
+
+    Scenario: Deve ser exibido um aviso quando não existir filmes cadastrados
+        When não existir filmes cadastrados 
+        Then um aviso será exibido
+
+
     # Scenario: Não deve ser possivel avançar ao chegar no fim da lista de filmes em destaque
-    #     When existir filmes cadastrados 
     # Scenario: Não deve ser possivel voltar ao chegar ao inicio da lista de filmes em destaque
-    #     When existir filmes cadastrados 
     # Scenario: Não deve ser possivel avançar ao chegar no fim da lista de filmes mais bem avaliados
-    #     When existir filmes cadastrados 
     # Scenario: Não deve ser possivel voltar ao chegar ao inicio da lista de filmes mais bem avaliados
-    #     When existir filmes cadastrados 
     # Scenario: Deve ser possivel visualizar a lista de filmes estando autenticado
     #     When está autenticado 
     #     Then deve ser possivel visualizar a lista de filmes 
--- testes-frontend/cypress/support/steps/listMovies.step.js
+++ testes-frontend/cypress/support/steps/listMovies.step.js
@@ -1,14 +1,9 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { faker } from "@faker-js/faker";
+import ListMoviesPage from "../pages/listMovies.page";
+let pageListMovies = new ListMoviesPage()
 
-// let movieCreated = {
-//     title: faker.music.songName(),
-//     genre: faker.music.genre(), 
-//     description: faker.lorem.words(), 
-//     durationInMinutes: 120, 
-//     releaseYear: 2020
-// }
-
+let movieId
 Given('usuário está na tela inicial', ()=>{
     cy.visit('https://raromdb-frontend-c7d7dc3305a0.herokuapp.com/')
     cy.url().should('eq', 'https://raromdb-frontend-c7d7dc3305a0.herokuapp.com/')
@@ -21,11 +16,28 @@ When('existir filmes cadastrados', ()=>{
             cy.fixture('movies.json').then((movies) => {
                 cy.adminCreatesALotOfMovies(movies, false);
               });
-              
         } else 
             return
     })
+
 })
+
+When('clicar no botão de próximo em filmes em destaque', ()=>{
+    pageListMovies.clickBtnProximoDestaque()
+})
+
+When('clicar no botão de anterior em filmes em destaque', ()=>{
+    pageListMovies.clickBtnProximoDestaque()
+})
+
+When('clicar no botão de próximo em filmes mais bem avaliados', ()=>{
+    pageListMovies.clickBtnProximoAvaliados()
+})
+
+When('clicar no botão de anterior em filmes mais bem avaliados', ()=>{
+    pageListMovies.clickBtnProximoAvaliados()
+})
+
 
 When('não existir filmes cadastrados', ()=>{
     cy.intercept('GET', 'https://raromdb-3c39614e42d4.herokuapp.com/api/movies', {
@@ -33,12 +45,54 @@ When('não existir filmes cadastrados', ()=>{
     })
 })
 
+When('clicar em um filme', ()=>{
+    cy.get(pageListMovies.firstMovieList).then((response)=>{
+        movieId = response.attr('href')
+        pageListMovies.clickFirstMovieList()
+    })
+})
+
+
+Then('usuário será redirecionado para a tela de detalhes do filme', ()=>{
+    cy.url().should('eq', `https://raromdb-frontend-c7d7dc3305a0.herokuapp.com` + movieId)
+})
+
+
 Then('um aviso será exibido', ()=>{
-    cy.contains('Ops! Parece que ainda não temos nenhum filme.')
+    cy.contains('Ops! Parece que ainda não temos nenhum filme.').should('be.visible')
 })
 
 Then('deve ser possivel visualizar duas categorias de filmes', ()=>{
     cy.contains('Filmes em destaque').should('be.visible')
     cy.contains('Mais bem avaliados').should('be.visible')
+})
+
+Then('deve ser possivel visualizar as informações dos filmes', ()=>{
+    cy.get('.carousel-data').find('a').each((cardMovie)=>{
+        cy.wrap(cardMovie).find('label').should('be.visible')
+        cy.wrap(cardMovie).find('.movie-details').should('be.visible')
+        cy.wrap(cardMovie).find('.movie-title').should('be.visible')
+        cy.wrap(cardMovie).find('p').should('be.visible')
+    })
+})
+
+Then('deve ser possivel visualizar mais filmes', ()=>{
+    cy.log('n sei ainda') 
+})
+
+Then('não deve ser possivel avançar', ()=>{
+    cy.get(pageListMovies.clickBtnProximoDestaque()).should('be.disabled')
+})
+
+Then('não deve ser possivel clicar no botão anterior de filmes em destaque', ()=>{
+    cy.get(pageListMovies.clickBtnAnteriorDestaque()).should('be.disabled')
+})
+
+Then('não deve ser possivel clicar no botão de próximo de filmes mais bem avaliados', ()=>{
+    cy.get(pageListMovies.clickBtnProximoAvaliados()).should('be.disabled')
+})
+
+Then('não deve ser possivel clicar no botão anterior de filmes mais bem avaliados', ()=>{
+    cy.get(pageListMovies.clickBtnAnteriorAvaliados()).should('be.disabled')
 })
 
