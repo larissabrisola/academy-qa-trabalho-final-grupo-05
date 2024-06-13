da74789f75c74c6c70439a76c73c5d5dda067f17
Ariana Silva <arianarebeca.silva@gmail.com>
2024-06-12 22:34:04 -0300

    feature steps movieDetailsQuery + review movies

--- testes-frontend/cypress/support/steps/movieDetailsQuery.step.js
+++ testes-frontend/cypress/support/steps/movieDetailsQuery.step.js
@@ -1,6 +1,49 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { faker } from "@faker-js/faker";
-import LoginPage from "../pages/login.page";
 import MovieDetailsPage from "../pages/movieDetails.page";
 
 const movieDetails = new MovieDetailsPage()
+
+Given ('que estou logado e na tela de filmes', () => {
+
+})
+
+Given('sou um usuário do tipo comum', () => {
+
+})
+
+When('selecionar um filme qualquer', () => {
+
+})
+
+Then('tenho acesso à todas aos detalhes do filme selecionado', () => {
+
+})
+
+Given('sou um usuário do tipo admin', () => {
+
+})
+
+Given('sou um usuário do tipo critico', () => {
+
+})
+
+Given('que estou na tela de filmes', () => {
+
+})
+
+Then('consigo visualizar a média das avaliações da audiência', () => {
+
+})
+
+Then('consigo visualizar a média das avaliações da crítica', () => {
+
+})
+
+Then('consigo criar uma avaliação', () => {
+
+})
+
+Then('consigo visualizar todas os detalhes de uma avaliação', () => {
+
+})
--- testes-frontend/cypress/support/steps/reviewMovie.step.js
+++ testes-frontend/cypress/support/steps/reviewMovie.step.js
@@ -3,3 +3,61 @@ import { faker } from "@faker-js/faker";
 import MovieDetailsPage from "../pages/movieDetails.page";
 
 const movieDetails = new MovieDetailsPage();
+
+Given('acesso à tela de filmes', () => {
+
+})
+
+When('selecionar um filme qualquer', () => {
+
+})
+
+When('criar uma nova avaliação', () => {
+
+})
+
+When('concluir operação', () => {
+
+})
+
+Then('será possível visualizar a avaliação criada', () => {
+
+})
+
+Then('não será possível criar uma avaliação', () => {
+
+})
+
+Given('que estou logado e na tela de um filme específico', () => {
+
+})
+
+When('atribuir uma nota', () => {
+    
+})
+
+Then('a nota é exibida e avaliação fica em branco', () => {
+
+})
+
+When('selecionar uma avaliação feita por mim anteriormente', () => {
+
+})
+
+When('inserir nova avaliação', () => {
+
+})
+
+Then('a avaliação antiga será atualizada', () => {
+
+})
+
+Then('não será possível criar nova avaliação', () => {
+
+})
+
+When('inserir um texto avaliativo com 500 caracteres', () => {
+
+})
+
+When('inserir um texto avaliativo com mais de 500 caracteres')
